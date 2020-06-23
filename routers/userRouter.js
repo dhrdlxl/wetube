fe33428afc29e32b1fe45ac03c48bb85710d9772
SeungHyunYoo <dhrdlxl@naver.com>
2020-06-24 01:47:15 +0900

    Change Profile HTML
    1. routes 경로 이름 수정
    
    2. userRouter userDetail을 가장 아래로 배치 : userDetail의 경로가 /:id 로 상위에 있으면 아래 경로를 모두 포함하는 문제 해결
    
    3. Change Profile 생성, changePassword를 a태그로 연결

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,9 +4,9 @@ import { users, userDetail, editProfile, changePassword } from "../controllers/u
 const userRouter = express.Router();
 
 userRouter.get(routes.users, users);
-userRouter.get(routes.user_detail, userDetail);
-userRouter.get(routes.edit_profile, editProfile);
-userRouter.get(routes.change_password, changePassword);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
 
 
 export default userRouter;
