af38d90c2c3bd847d77074c74751b2596b4d2159
SeungHyunYoo <dhrdlxl@naver.com>
2020-06-24 21:31:19 +0900

    Home Controller part One
    Get data from temporary DB

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id:123123,
+        title: "Video good",
+        description: "Video from ysh",
+        views:24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "SeungHyun",
+            email: "example@gmail.com"
+        }
+    },
+    {
+        id:321321321,
+        title: "Video nice",
+        description: "Video from ysh",
+        views:4,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "SeungHyun",
+            email: "example@gmail.com"
+        }
+    },
+    {
+        id:9876123,
+        title: "Video great",
+        description: "Video from ysh",
+        views:22,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "SeungHyun",
+            email: "example@gmail.com"
+        }
+    },
+    {
+        id:71324,
+        title: "Video perfect",
+        description: "Video from ysh",
+        views:12,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "SeungHyun",
+            email: "example@gmail.com"
+        }
+    },
+]
