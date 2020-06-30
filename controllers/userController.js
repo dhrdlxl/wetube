a6d5093c0960b6492f800cf0c01c3c249ac70c6a
SeungHyunYoo <dhrdlxl@naver.com>
2020-06-30 18:37:49 +0900

    Video Controller
    Make Upload
    
    Add required option to login, join, upload

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,6 +17,7 @@ export const postJoin = (req, res) => {
         res.redirect(routes.home);
     }
 };
+
 export const getLogin = (req, res) => {
     res.render("login", {pageTitle: "Login"});
 };
@@ -24,7 +25,12 @@ export const postLogin = (req, res) => {
     //To Do : If error occur show login page
     res.redirect(routes.home);
 };
-export const logout = (req, res) => res.render("logout", {pageTitle: "Logout"});
+
+export const logout = (req, res) => {
+    //To Do : Process Logout
+    res.redirect(routes.home);
+}
+
 export const users = (req, res) => res.render("users", {pageTitle: "Users"});
 export const userDetail = (req, res) => res.render("userDetail", {pageTitle: "User Detail"});
 export const editProfile = (req, res) => res.render("editProfile", {pageTitle: "Edit Profile"});
